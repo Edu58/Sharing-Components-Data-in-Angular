d302356ce81938f8a42a76cdcb4e666aae8a45b1
Edu58 <edumuriithi58@gmail.com>
2022-03-19 19:56:30 +0300

    adds @Output()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,11 @@ export class AppComponent {
   userName: string = 'EDWIN KARIMI'
   message: string = 'I am the best developer your company will ever see'
 
+  receivedMessage!: string;
+
+  printMessage (usermessage: any):void {
+    this.receivedMessage = usermessage
+    console.log(usermessage)
+  }
+
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -13,9 +13,15 @@ export class CardComponent implements OnInit {
   @Input()
   message!: string
 
+  @Output() userInput: EventEmitter<string> = new EventEmitter()
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  sendItems (value: string): void {
+    this.userInput.emit(value)
   }
 
 }
